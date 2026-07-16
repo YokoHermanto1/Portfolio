1cea340b76aed6fa849048d0f0fd3d77479c2fe2
YokoHermanto1 <yokohermanto@gmail.com>
2026-07-16 20:00:48 +0700

    Changeing Layout Hero.tsx

--- FRONTEND/src/pages/Hero.tsx
+++ FRONTEND/src/pages/Hero.tsx
@@ -14,11 +14,11 @@ function Hero() {
   ];
 
   return (
-    <section id="hero" className="pt-20 pb-15 font-serif md:px-10 text-center md:text-left">
+    <section id="hero" className="pt-20 pb-15 font-serif md:px-10 text-center md:text-left ">
       {/* Parent Container */}
       <div className="mx-auto w-full max-w-7xl sm:px-8 lg:px-0 px-4 py-10 grid grid-cols-1 gap-10 lg:grid-cols-[1fr_auto] items-center">
         {/* Description & CTA Button */}
-        <div className="space-y-9 lg:text-left sm:order-1 lg:order-0 ">
+        <div className="space-y-9 lg:text-left sm:order-1 lg:order-0 md:text-center">
           <h1
             aria-label="Yoko Hermanto"
             className="font-serif font-normal leading-none text-[clamp(4rem,10vw,8rem)]"
