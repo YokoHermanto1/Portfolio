19d922a7af2092d46b0d2829720257a0fed3653c
YokoHermanto1 <yokohermanto@gmail.com>
2026-07-17 12:01:42 +0700

    Href Navbar Toolkit

--- FRONTEND/src/components/Navbar.tsx
+++ FRONTEND/src/components/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
   const menuNavbar = [
     { id: "Background", title: "Background", href: "#Background" },
     { id: "projects", title: "Projects", href: "#Projects" },
-    { id: "toolkit", title: "Toolkit", href: "#toolkit" },
+    { id: "toolkit", title: "Toolkit", href: "#ToolkitItems" },
     { id: "contact", title: "Contact", href: "#contact" },
   ];
   return (
